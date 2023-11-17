1655136eeebea4b200b14950d9ba9e848155e710
maorbeber <maorbber@gmail.com>
2023-11-17 21:35:52 +0500

    create the design of add health card service and photo id service

---
+++ app/src/main/java/com/example/novigradservice/AdminScreens/AddHealthCardServiceActivity.java
@@ -0,0 +1,16 @@
+package com.example.novigradservice.AdminScreens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.novigradservice.R;
+
+public class AddHealthCardServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_health_card_service);
+    }
+}
---
+++ app/src/main/java/com/example/novigradservice/AdminScreens/AddPhotoIdServiceActivity.java
@@ -0,0 +1,16 @@
+package com.example.novigradservice.AdminScreens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.novigradservice.R;
+
+public class AddPhotoIdServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_photo_id_service);
+    }
+}
